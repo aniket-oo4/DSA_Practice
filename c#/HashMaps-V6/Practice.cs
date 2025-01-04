59378ac0e92b38e9c264e298dc7f8e58afb27c40
aniket <aniketvarkhade23@gmail.com>
2025-01-04 15:02:58 +0530

    competitive programming  practice Folder added

---
+++ c#/HashMaps-V6/Practice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace problems
+{
+    class Program
+    {
+        public static void IsEquallyDistributable(int w)
+        {
+            Console.Write((w % 2 == 0 && w != 0) ? "YES" : "NO");
+        }
+
+        public static void Main(String[] args)
+        {
+            int weight = int.Parse(Console.ReadLine());
+            IsEquallyDistributable(weight);
+        }
+
+    }
+
+}
